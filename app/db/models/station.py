916456d00c63e9755fd7195e097ffd6557da8cee
Pamela <pamela.benavides.loaiza@gmail.com>
2025-03-12 16:49:10 -0300

    refactor: Update Station model to allow nullable fields and reorganize relationships

--- app/db/models/station.py
+++ app/db/models/station.py
@@ -6,6 +6,7 @@ from app.db.base import Base
 
 class Station(Base):
     __tablename__ = "stations"
+
     stationid = Column(Integer, primary_key=True, index=True)
     campaignid = Column(Integer, ForeignKey("campaigns.campaignid"), nullable=True)
     stationname = Column(String, unique=True)
@@ -13,7 +14,9 @@ class Station(Base):
     description = Column(String, nullable=True)
     contactname = Column(String, nullable=True)
     contactemail = Column(String, nullable=True)
-    active = Column(Boolean, default=True)
-    startdate = Column(DateTime)
+    active = Column(Boolean, nullable=True)
+    startdate = Column(DateTime, nullable=True)
+    
+    #relationships
     sensor = relationship("Sensor", lazy="joined")
-    # location = relationship("Locations", lazy="joined")
+    #location = relationship("Location", lazy="joined")
