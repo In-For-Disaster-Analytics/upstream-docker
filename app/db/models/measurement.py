5c4ac3dc268f89da16b1a6aaafb656947ce27ed9
Pamela <pamela.benavides.loaiza@gmail.com>
2025-03-12 16:55:48 -0300

    refactor: Update Measurement model to allow nullable fields and reorganize relationships

--- app/db/models/measurement.py
+++ app/db/models/measurement.py
@@ -6,13 +6,16 @@ from app.db.base import Base
 
 class Measurement(Base):
     __tablename__ = "measurements"
+
     measurementid = Column(Integer, primary_key=True, index=True)
-    sensorid = Column(Integer, ForeignKey("sensors.sensorid"))
-    stationid = Column(Integer)
-    variablename = Column(String)
-    collectiontime = Column(DateTime)
+    sensorid = Column(Integer, ForeignKey("sensors.sensorid"), nullable=True)
+    stationid = Column(Integer, nullable=True)
+    variablename = Column(String, nullable=True)
+    collectiontime = Column(DateTime, nullable=True)
     variabletype = Column(String, nullable=True)
     description = Column(String, nullable=True)
     measurementvalue = Column(Float, nullable=True)
-    locationid = Column(Integer, ForeignKey("locations.locationid"))
+    locationid = Column(Integer, ForeignKey("locations.locationid"), nullable=True)
+
+    #relationships
     location = relationship("Location", lazy="joined")
