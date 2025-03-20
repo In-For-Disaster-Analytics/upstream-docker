6719fadf982ba11e2bb3ced79646b7dc0537968d
Maximiliano Osorio <maxiosorio@gmail.com>
2025-03-20 10:25:25 -0300

    feat: add OpenAPI generation script and initial OpenAPI specification
    - Introduced a new script `create_openapi.py` to generate OpenAPI documentation for the FastAPI application, saving the output to `openapi.json`.
    - Created the initial `openapi.json` file containing the OpenAPI specification for the API, detailing endpoints, request parameters, and response schemas.
    - Enhanced the main application by adding CORS middleware to allow cross-origin requests, improving API accessibility.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.v1.main import api_router
+
 
 load_dotenv()
 
@@ -13,6 +15,16 @@ app = FastAPI(
         "name": "Will Mobley",
         "email": "wmobley@tacc.utexas.edu",
     },
+
+)
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
 )
 
 app.include_router(api_router, prefix="/api/v1")
