3e86184e62f3d827919167086f973448b58aea2e
vkren <119969393+vkren@users.noreply.github.com>
2025-04-11 11:50:21 -0500

    refined upload_csv with bulk_insert_mappings (#80)
    Co-authored-by: Vlad Krendelev <vkren@vkren-mbp22.local>

--- app/api/v1/routes/upload_file/upload_csv.py
+++ app/api/v1/routes/upload_file/upload_csv.py
@@ -32,98 +32,122 @@ def post_sensor_and_measurement(
     upload_file_sensors: Annotated[UploadFile, File(description="File with sensors.")],
     upload_file_measurements: Annotated[UploadFile, File(description="File with measurements.")],
     current_user: User = Depends(get_current_user),
-) -> Dict[str, bool | float | str ]:
+) -> Dict[str, bool | float | str | datetime]:
     t1 = time.time()
     time_file_received = datetime.now()
     print(f'The files are uploaded to the system at {time_file_received}')
-    uploadevent = UploadFileEvent(time=time_file_received)
-
     # Initialiaze a response dictionary
     response = {}
-
     # Initialize an empty dictionary and list to store Sensor and Measurement objects
-    sensors_objs = dict()
-    meas_objs_all = []
-
+    #sensors_objs = dict()
+    sensor_mapps = [] 
+    meas_dicts_all = []
+    
     response['uploaded_file_sensors stored in memory'] = upload_file_sensors._in_memory
     response['uploaded_file_measurements stored in memory'] = upload_file_measurements._in_memory
+    
 
-    # Open and process the first file - upload_file_sensors
-    text_wrapper = TextIOWrapper(upload_file_sensors.file, encoding='utf-8',)
-    sensor_data = csv.DictReader(text_wrapper)
+    # open the session with database
+    with SessionLocal() as session:
+        # Create an UploadFileEvent intance and commit
+        # to extract id for future reference 
+        uploadevent = UploadFileEvent(time=time_file_received)
+        session.add(uploadevent)
+        session.commit()
 
-    for sd in sensor_data:
+        # Open and process the first file - upload_file_sensors
+        text_wrapper = TextIOWrapper(upload_file_sensors.file, encoding='utf-8',)
+        sensor_data = csv.DictReader(text_wrapper)
 
-        try:
-            sensor = SensorCSV.model_validate(sd)
-        except ValidationError as exc:
-            error_messages = [f"Field {err['loc'][0]}: {err['msg']}" for err in exc.errors()]
-            raise HTTPException(status_code=400, detail=str(error_messages))
-
-        sensor_dict = sensor.model_dump()
-        if sensor_dict['variablename'] is None:
-            sensor_dict['variablename'] = 'No BestGuess Formula'
-
-        sensor_dict['upload_file_event'] = uploadevent
-        sensors_objs[sensor.alias] = Sensor(**sensor_dict)
-
-    upload_file_sensors.file.close()
-
-    # Open and process the second file - upload_file_measurements
-    text_wrapper = TextIOWrapper(upload_file_measurements.file, encoding='utf-8-sig', errors='replace')
-    measurement_data = csv.DictReader(text_wrapper)
-
-    # iterate over each dictionary(i.e., row) in measurement data
-    counter = 0
-    for md in measurement_data:
-
-        counter += 1
-        if counter % 100 == 0:
-            print(counter)
-
-        # Validate collection_time and location with Pydantic
-        try:
-            collection_time = CollTimeCSV(collection_time=md['collectiontime']) # md['Time_CDT'])
-            location = LocationCSV(long_deg=md['Lon_deg'], lat_deg=md['Lat_deg'])
-        except ValidationError as exc:
-            error_messages = [f"Field {err['loc'][0]}: {err['msg']}" for err in exc.errors()]
-            raise HTTPException(status_code=400, detail=str(error_messages))
-
-        # Process location data and create an instance of Locations
-        loc_geometry = f"Point ({location.long_deg} {location.lat_deg})"
-        loc_geometry = WKTElement(loc_geometry, srid=4326)
-        # iterate over each alias in sensors_objs
-        for al in sensors_objs.keys():
-
-            # Validate measurement_value with Pydantic
+        # process each row(dict) from sensor_data
+        for sd in sensor_data:
+            # validate the row(dict) with Pydantic
             try:
-                measurement_value = MeasurementCSV(measurement_value=md[al])
+                sensor = SensorCSV.model_validate(sd)
             except ValidationError as exc:
                 error_messages = [f"Field {err['loc'][0]}: {err['msg']}" for err in exc.errors()]
                 raise HTTPException(status_code=400, detail=str(error_messages))
 
+            sensor_dict = sensor.model_dump()
+            if sensor_dict['variablename'] is None:
+                sensor_dict['variablename'] = 'No BestGuess Formula'
 
-            # Create an instance of Measurement
-            measurement = Measurement(
-                stationid=station_id,
-                collectiontime=collection_time.collection_time,
-                measurementvalue=measurement_value.measurement_value,
-                geometry=loc_geometry,
-                sensor=sensors_objs[al],
-                upload_file_event=uploadevent
-            )
-
-            meas_objs_all.append(measurement)
-
-    upload_file_measurements.file.close()
-
-    t2 = time.time() # Timestamp when the data processing is complete.
-    print('Data processing step is complete.')
-
-    with SessionLocal() as session:
-        session.add_all(meas_objs_all)
+            # add stationid
+            sensor_dict['stationid'] = station_id
+            # add upload_file_events_id for reference
+            sensor_dict['upload_file_events_id'] = uploadevent.id
+            # append a dict with attributes of a sensor to sensor_mapps to be commited
+            sensor_mapps.append(sensor_dict)
+            
+        # add all Sensor objects to the session
+        session.bulk_insert_mappings(Sensor, sensor_mapps)
+        # commit sensors to the database
         session.commit()
+        
+        # query aliases and sensor ids that have been commited
+        alias_to_sensorid = session.query(
+            Sensor.alias, Sensor.sensorid
+        ).filter(Sensor.upload_file_events_id==uploadevent.id).all()
+        
+        # reformat the alias_to_sensorid into a {alias: sensorid} map
+        alias_to_sensorid_map = {el.alias: el.sensorid for el in alias_to_sensorid}
+        
+        upload_file_sensors.file.close()
 
+        # Open and process the second file - upload_file_measurements
+        text_wrapper = TextIOWrapper(upload_file_measurements.file, encoding='utf-8-sig', errors='replace')
+        measurement_data = csv.DictReader(text_wrapper)
+
+        # iterate over each dictionary(i.e., row) in measurement data
+        counter = 0
+        for md in measurement_data:
+
+            counter += 1
+            if counter % 100 == 0:
+                print(counter)
+
+            # Validate collection_time and location with Pydantic
+            try:
+                collection_time = CollTimeCSV(collection_time=md['collectiontime']) # md['Time_CDT'])
+                location = LocationCSV(long_deg=md['Lon_deg'], lat_deg=md['Lat_deg'])
+            except ValidationError as exc:
+                error_messages = [f"Field {err['loc'][0]}: {err['msg']}" for err in exc.errors()]
+                raise HTTPException(status_code=400, detail=str(error_messages))
+
+            # Process location data and create an instance of Locations
+            loc_geometry = f"Point ({location.long_deg} {location.lat_deg})"
+            loc_geometry = WKTElement(loc_geometry, srid=4326)
+            # iterate over each alias in sensors_objs
+            for alias in alias_to_sensorid_map.keys():
+
+                # Validate measurement_value with Pydantic
+                try:
+                    measurement_value = MeasurementCSV(measurement_value=md[alias])
+                except ValidationError as exc:
+                    error_messages = [f"Field {err['loc'][0]}: {err['msg']}" for err in exc.errors()]
+                    raise HTTPException(status_code=400, detail=str(error_messages))
+                
+                # create a dict with mesurement's attributes
+                measurement_dict = {
+                    'stationid': station_id,
+                    'collectiontime': collection_time.collection_time,
+                    'measurementvalue':measurement_value.measurement_value,
+                    'geometry': loc_geometry,
+                    'sensorid': alias_to_sensorid_map[alias],
+                    'upload_file_events_id': uploadevent.id
+                }
+                # Store measurement_dicts to commit later
+                meas_dicts_all.append(measurement_dict)
+
+        upload_file_measurements.file.close()
+
+        t2 = time.time() # Timestamp when the data processing is complete.
+        print('Data processing step is complete.')
+
+        # add and commit the measurements
+        session.bulk_insert_mappings(Measurement, meas_dicts_all)
+        session.commit()
+    
     t3 = time.time()
 
     response['Data Processing time'] = f"{round(t2-t1, 1)} seconds."
@@ -132,5 +156,5 @@ def post_sensor_and_measurement(
     print(t2-t1)
     print(t3-t2)
     print(response)
-
+    
     return response
