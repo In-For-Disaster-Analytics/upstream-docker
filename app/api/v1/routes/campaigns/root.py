e266a100907488798ca510595b207b799e89f19b
Maximiliano Osorio <maxiosorio@gmail.com>
2025-07-17 16:02:33 -0400

    fix: format

--- app/api/v1/routes/campaigns/root.py
+++ app/api/v1/routes/campaigns/root.py
@@ -2,13 +2,18 @@ from datetime import datetime
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException, Query, Response
-from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 from app.api.dependencies.pytas import check_allocation_permission
 
 from app.api.dependencies.auth import get_current_user
 from app.api.dependencies.pytas import get_allocations
-from app.api.v1.schemas.campaign import CampaignCreateResponse, GetCampaignResponse, ListCampaignsResponsePagination, CampaignsIn, CampaignUpdate
+from app.api.v1.schemas.campaign import (
+    CampaignCreateResponse,
+    GetCampaignResponse,
+    ListCampaignsResponsePagination,
+    CampaignsIn,
+    CampaignUpdate,
+)
 from app.api.v1.schemas.user import User
 from app.db.repositories.campaign_repository import CampaignRepository
 from app.db.session import get_db
@@ -17,23 +22,38 @@ from app.services.campaign_service import CampaignService
 
 router = APIRouter(prefix="/campaigns", tags=["campaigns"])
 
+
 @router.post("")
-async def create_campaign(campaign: CampaignsIn,
-                         current_user: User = Depends(get_current_user), 
-                         db: Session = Depends(get_db)) -> CampaignCreateResponse:
+async def create_campaign(
+    campaign: CampaignsIn,
+    current_user: User = Depends(get_current_user),
+    db: Session = Depends(get_db),
+) -> CampaignCreateResponse:
     campaign_service = CampaignService(CampaignRepository(db))
     return campaign_service.create_campaign(campaign)
+
 
 @router.get("")
 async def list_campaigns(
     page: int = 1,
     limit: int = 20,
-    bbox: Annotated[str | None, Query(description="Bounding box of the campaign west,south,east,north")] = None,
-    start_date: Annotated[datetime | None, Query(description="Start date of the campaign", example="2024-01-01")] = None,
-    end_date: Annotated[datetime | None, Query(description="End date of the campaign", example="2025-01-01")] = None,
-    sensor_variables: Annotated[list[str] | None, Query(description="List of sensor variables to filter by")] = None,
+    bbox: Annotated[
+        str | None,
+        Query(description="Bounding box of the campaign west,south,east,north"),
+    ] = None,
+    start_date: Annotated[
+        datetime | None,
+        Query(description="Start date of the campaign", example="2024-01-01"),
+    ] = None,
+    end_date: Annotated[
+        datetime | None,
+        Query(description="End date of the campaign", example="2025-01-01"),
+    ] = None,
+    sensor_variables: Annotated[
+        list[str] | None, Query(description="List of sensor variables to filter by")
+    ] = None,
     current_user: User = Depends(get_current_user),
-    db: Session = Depends(get_db)
+    db: Session = Depends(get_db),
 ) -> ListCampaignsResponsePagination:
     allocations = get_allocations(current_user.username)
     campaign_service = CampaignService(CampaignRepository(db))
@@ -49,10 +69,13 @@ async def list_campaigns(
     )
     return response
 
+
 @router.get("/{campaign_id}")
-async def get_campaign(campaign_id: int,
-                    current_user: User = Depends(get_current_user), 
-                    db: Session = Depends(get_db)) -> GetCampaignResponse:
+async def get_campaign(
+    campaign_id: int,
+    current_user: User = Depends(get_current_user),
+    db: Session = Depends(get_db),
+) -> GetCampaignResponse:
     campaign_service = CampaignService(CampaignRepository(db))
     campaign = campaign_service.get_campaign_with_summary(campaign_id)
     if not campaign:
@@ -73,6 +96,7 @@ def delete_sensor(
     campaign_service.delete_campaign(campaign_id=campaign_id)
     return Response(status_code=204)
 
+
 @router.put("/{campaign_id}", response_model=CampaignCreateResponse)
 def update_campaign(
     campaign_id: int,
@@ -87,6 +111,7 @@ def update_campaign(
     if not updated_campaign:
         raise HTTPException(status_code=404, detail="Campaign not found")
     return updated_campaign
+
 
 @router.patch("/{campaign_id}", response_model=CampaignCreateResponse)
 def partial_update_campaign(
